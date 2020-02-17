911cf9cf0e4331cee5b02ef1b32db0cb3b8c91cf
Natasha Robarge <robarge.natasha7@gmail.com>
2020-02-17 10:58:29 -0600

    add google login auth and redirect to signup

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import firebase from 'firebase';
 
 import Firebase from '../FirebaseConfig';
 import '../Stylesheets/Login.css';
@@ -6,9 +7,16 @@ import '../Stylesheets/Login.css';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [token, setToken] = useState();
+  const [user, setUser] = useState();
 
   const loginWithGoogle = () => {
+    let provider = new firebase.auth.GoogleAuthProvider();
 
+    Firebase.auth().signInWithPopup(provider).then(function(result) {
+      setToken(result.credential.accessToken);
+      setUser(result.user);
+    })
   }
 
   const loginWithEmail = async () => {
@@ -38,11 +46,13 @@ const Login = () => {
         <button className="login-btn" onClick={() => loginWithEmail()}>Login</button>
       </div>
       <div className="google-login-container">
-        <button className="google-btn">
+        <button className="google-btn" onClick={() => loginWithGoogle()}>
           <img width="20px" alt="Google sign-in" 
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
           <p>Sign in With Google</p>
         </button>
+      
+        <a href="/signup" className="signup-link">Don't have an account? Sign up for one here</a>
       </div>
     </div>
   )
