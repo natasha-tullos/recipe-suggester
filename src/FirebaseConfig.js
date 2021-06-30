064e1a2b3ba1907459ddf6bde0090982536b3244
Natasha Robarge <robarge.natasha7@gmail.com>
2021-06-30 13:23:36 -0500

    update key names for env file

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: process.env.REACT_APP_FIRE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
